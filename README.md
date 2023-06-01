da84de20b5d9f606d01c95bdec31cd7c676d31de
Shihan Chowdhury <hackedgdown@gmail.com>
2023-06-01 15:42:30 +0100

    78-6 Make User Admin and Install JWT

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # ML-12-Module-77 [Final Project Part 4]
 * 78-3 Save user if he doesn't exits in the DB
 * 78-5 Display All users and Create Make Admin API
+* 78-6 Make User Admin and Install JWT
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
 app.use(cors());
 app.use(express.json());
